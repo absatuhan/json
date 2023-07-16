c23eb4887a8eb83458f0e42811b3d1e614c02bbb
Ahmet Batuhan Sofu <130096667+absatuhan@users.noreply.github.com>
2023-07-16 21:10:31 +0300

    Add files via upload

---
+++ Personel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    internal class Personel
+    {
+        public Guid Id { get; set; }
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public string EmailAdres { get; set; }
+        public string TelefonNumarasi { get; set; }
+        public string Sehir { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,18 +5,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JsonOkuma1
+namespace Json
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string JsonOkunanData=File.ReadAllText("c://JsonIslemlerim//Personellerim.json");
-            List<XObject> Data=Newtonsoft.Json.JsonConvert.DeserializeObject<List<XObject>>(JsonOkunanData);
-            for (int i = 0; i < Data.Count; i++)
+            List<Personel> Personellerim = new List<Personel>();
+            for (int i = 0; i < 1000; i++)
             {
-                Console.WriteLine(Data[i]);
+                Personel p1 = new Personel();
+                p1.Id=Guid.NewGuid();
+                p1.Isim=FakeData.NameData.GetFirstName();
+                p1.Soyisim=FakeData.NameData.GetSurname();
+                p1.EmailAdres = $"{p1.Isim}.{p1.Soyisim}@{FakeData.NetworkData.GetDomain()}";
+                p1.TelefonNumarasi=FakeData.PhoneNumberData.GetPhoneNumber();
+                Personellerim.Add(p1);              
             }
+            Console.WriteLine("Bilgileriniz Json formatinda C://JsonIslemlerim//Personellerim.json olarak kayit edilecektir");
+            if (Directory.Exists("c://JsonIslemlerim//"))
+            {
+                // ilgili klasor varsa herhangi bir islem yapmiyoruz
+            }
+            else
+            {
+                Directory.CreateDirectory("c://JsonIslemlerim//");
+            }
+            string JsonPersonellerim = Newtonsoft.Json.JsonConvert.SerializeObject(Personellerim);
+            File.WriteAllText("c://JsonIslemlerim//Personellerim.json", JsonPersonellerim);
+
+            Console.WriteLine("Json export islemi tamamlandi");
+
 
         }
     }

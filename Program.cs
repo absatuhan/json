5a51a7017869eccd76e51d788670bc8c4f65df34
Ahmet Batuhan Sofu <130096667+absatuhan@users.noreply.github.com>
2023-07-16 21:10:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonOkuma1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string JsonOkunanData=File.ReadAllText("c://JsonIslemlerim//Personellerim.json");
+            List<XObject> Data=Newtonsoft.Json.JsonConvert.DeserializeObject<List<XObject>>(JsonOkunanData);
+            for (int i = 0; i < Data.Count; i++)
+            {
+                Console.WriteLine(Data[i]);
+            }
+
+        }
+    }
+}
